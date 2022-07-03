071528e9113c99ea64314ea09a1a1b8a25f51529
tobor <rosborne@osbornepro.com>
2022-07-03 11:57:56 -0600

    Update Set-AOVPNServerProfile.ps1

--- Set-AOVPNServerProfile.ps1
+++ Set-AOVPNServerProfile.ps1
@@ -1,6 +1,21 @@
 # The below commands are used to enable modern encryption algorithms on a Windows Server with Always On VPN
 # Be sure to issue the matching commands on the client side which I have in my New-AOVPNClientProfile.ps1 script in this same GitHub repository
 
+$Logo = @"
+╔═══╗░░╔╗░░░░░░░░░░░░╔═══╗░░░░░
+║╔═╗║░░║║░░░░░░░░░░░░║╔═╗║░░░░░
+║║░║╠══╣╚═╦══╦═╦═╗╔══╣╚═╝╠═╦══╗
+║║░║║══╣╔╗║╔╗║╔╣╔╗╣║═╣╔══╣╔╣╔╗║
+║╚═╝╠══║╚╝║╚╝║║║║║║║═╣║░░║║║╚╝║
+╚═══╩══╩══╩══╩╝╚╝╚╩══╩╝░░╚╝╚══╝
+===============================
+If you can't beat `em tech `em!
+===============================
+https://osbornepro.com
+EMAIL: info@osbornepro.com
+"@
+Write-Output "$Logo"
+
 Set-VpnServerConfiguration -CustomPolicy -AuthenticationTransformConstants SHA256128 -CipherTransformConstants AES128 -DHGroup Group14 -EncryptionMethod AES128 -IntegrityCheckMethod SHA256 -PFSgroup PFS2048 -SALifeTimeSeconds 28800 -MMSALifeTimeSeconds 86400 -SADataSizeForRenegotiationKilobytes 1024000
 
 $Thumbprint = 'Root CA Certificate Thumbprint'
